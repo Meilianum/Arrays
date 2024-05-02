65c4fcf924e99facc7dd7f3afc9264c3281314d3
Meilianum <164498186+Meilianum@users.noreply.github.com>
2024-05-02 10:37:39 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+let biography =["Anus","10","karachi","master" ]
+biography.unshift('ayan')
+biography.shift()
+biography.push("anzila")
+biography.pop()
+console.log(biography)
+
+let  visittrvellingTing =["Australia" ,'Newzeland', "Srilanka", "Canada"]
+visittrvellingTing. unshift("Suadi arbia")
+visittrvellingTing.push('Iceland')
+visittrvellingTing.pop()
+console.log(visittrvellingTing)
+
+let familyName =[ "Robina  50" , "Rukhsana  22", "shabana  33", "Sabeena 18"]
+
+console.log(familyName[1]);
+
+let visitingSeaside = ["Horseridinf", "Camelridind", "trvillinship"]
+visitingSeaside.unshift("Renthouse")
+visitingSeaside.push("Enjoing party")
+visitingSeaside.pop()
+visitingSeaside.shift()
+
+console.log(visitingSeaside)
+
+let mybioData =["name" , "Rukhsana",  "age", "21" ,"education" ,"Master"]
+console.log(mybioData)
+
+
+// let myfriendName = ["Ayesha", "amber","soni", "anbiya", "sahar"]
+// myfriendName.unshift('waniya')
+// myfriendName.push('anum')
+// myfriendName.pop()
+// console.log(myfriendName)
